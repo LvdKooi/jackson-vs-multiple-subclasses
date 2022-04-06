c3597e77abdccec6d44f3327b2b77137d2aeef5c
Laurens van der Kooi <laurens.van.der.kooi@ordina.nl>
2022-04-06 10:18:30 +0200

    upgrading dependencies, upgrading swagger configuration, cleaning up API-spec

--- vehicle/src/main/java/nl/kooi/vehicle/SwaggerConfig.java
+++ vehicle/src/main/java/nl/kooi/vehicle/SwaggerConfig.java
@@ -2,20 +2,16 @@ package nl.kooi.vehicle;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
- * Created by Laurens van der Kooi on 10/10/19.
- */
 
 @Configuration
-@EnableSwagger2
+@EnableWebMvc
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
@@ -26,15 +22,5 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .paths(PathSelectors.any())
                 .build();
     }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
 }
-
 
