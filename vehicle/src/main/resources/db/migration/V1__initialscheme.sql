4d461f000bebc40ce15d724a0c07ed3d8a639a2a
Laurens van der Kooi <laurens.van.der.kooi@ordina.nl>
2022-04-06 16:04:23 +0200

    adding extra descriptions to api.yaml, cleaning up V1__initialscheme.sql

--- vehicle/src/main/resources/db/migration/V1__initialscheme.sql
+++ vehicle/src/main/resources/db/migration/V1__initialscheme.sql
@@ -1,37 +1,51 @@
-create table vehicle (
-       id bigint primary key AUTO_INCREMENT,
-       vehicle_type varchar(255),
-      model varchar(255),
-      brand varchar(255)
-);
+CREATE TABLE vehicle
+  (
+     id           BIGINT PRIMARY KEY auto_increment,
+     vehicle_type VARCHAR(255) NOT NULL,
+     model        VARCHAR(255),
+     brand        VARCHAR(255)
+  );
 
-create table motorized_vehicle (
- id bigint,
- license_plate varchar(255),
- horse_power int,
-   fuel_type varchar(255)
-);
+CREATE TABLE motorized_vehicle
+  (
+     id            BIGINT NOT NULL,
+     license_plate VARCHAR(255),
+     horse_power   INT,
+     fuel_type     VARCHAR(255)
+  );
 
-create table car (
- id bigint,
-   number_of_doors int,
-    body_style varchar(255)
-);
+CREATE TABLE car
+  (
+     id              BIGINT NOT NULL,
+     number_of_doors INT,
+     body_style      VARCHAR(255)
+  );
 
-create table bus (
- id bigint,
-    bus_type varchar(255),
-    liters_luggage_capacity int
-);
+CREATE TABLE bus
+  (
+     id                      BIGINT NOT NULL,
+     bus_type                VARCHAR(255),
+     liters_luggage_capacity INT
+  );
 
-create table watercraft (
- id bigint,
-    propulsion_type varchar(255),
-    watercraft_type varchar(255),
-    length_meters int,
-    draft_meters int
-);
+CREATE TABLE watercraft
+  (
+     id              BIGINT NOT NULL,
+     propulsion_type VARCHAR(255),
+     watercraft_type VARCHAR(255),
+     length_meters   INT,
+     draft_meters    INT
+  );
 
+ALTER TABLE motorized_vehicle
+  ADD CONSTRAINT motorized_vehicle_vehicle_id_fk FOREIGN KEY (id) REFERENCES
+  vehicle(id);
 
+ALTER TABLE car
+  ADD CONSTRAINT car_vehicle_id_fk FOREIGN KEY (id) REFERENCES vehicle(id);
 
+ALTER TABLE bus
+  ADD CONSTRAINT bus_vehicle_id_fk FOREIGN KEY (id) REFERENCES vehicle(id);
 
+ALTER TABLE watercraft
+  ADD CONSTRAINT watercraft_vehicle_id_fk FOREIGN KEY (id) REFERENCES vehicle(id);
